0142a850986d0bf851d9d321c4fb4956d7652984
Nhat Pham <nhat2up@gmail.com>
2022-12-29 08:05:04 +0700

    'start'

---
+++ spring-data-jpa-course/src/test/java/com/nhattpam/springdatajpacourse/SpringDataJpaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhattpam.springdatajpacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
